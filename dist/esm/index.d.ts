a19fdab42b71b23898acd8b2a80853368f145828
Braian Rosas <axis.dev@outlook.com.ar>
2021-09-26 18:00:58 -0300

    fix types export

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -1,2 +1,4 @@
 import Validator from "./Validator/Validator";
+import { strVal } from "./types/types";
 export default Validator;
+export { strVal };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import Validator from "./Validator/Validator";
+import { strVal } from "./types/types";
 
 export default Validator;
+
+export { strVal };
